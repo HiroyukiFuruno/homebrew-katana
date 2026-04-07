eaf0512479e76b055578b36db6eb3af427af48c1
Hiroyuki Furuno <58550284+HiroyukiFuruno@users.noreply.github.com>
2026-04-07 19:30:52 +0900

    chore: update katana-desktop@0.16.3 to 0.16.3

--- Casks/katana-desktop@0.16.3.rb
+++ Casks/katana-desktop@0.16.3.rb
@@ -1,6 +1,6 @@
 cask "katana-desktop@0.16.3" do
   version "0.16.3"
-  sha256 "81b47ec78f1a2d0c4cb7bad811f3a2057210fbf6fe468998f077e05f4ee72a22"
+  sha256 "6fdf65bef3b261663f7eed4ac47342f24bf5825bc797a84d39b5a39d671e5ee8"
 
   url "https://github.com/HiroyukiFuruno/KatanA/releases/download/v#{version}/KatanA-Desktop-#{version}.dmg"
   name "KatanA Desktop"
