a3cd5ce309ee52af0bc75dc047ea2fc5e90994c8
Hiroyuki Furuno <58550284+HiroyukiFuruno@users.noreply.github.com>
2026-04-10 15:22:42 +0900

    chore: update katana-desktop to 0.18.1

--- Formula/katana-desktop.rb
+++ Formula/katana-desktop.rb
@@ -4,7 +4,7 @@ class KatanaDesktop < Formula
   version "0.18.1"
 
   url "https://github.com/HiroyukiFuruno/KatanA/releases/download/v0.18.1/KatanA-linux-x86_64.tar.gz"
-  sha256 "1cb35cb58276087e195c260f7b289f483ae9a1ab9674f8d770c096c348e0f985"
+  sha256 "0e1237fa69821fd6dd44cc3e3b18b4f07cb03a66d4593af40cd8bd4d84c5f969"
 
   depends_on :linux
 
