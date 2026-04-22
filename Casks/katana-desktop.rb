494a24f5c757bf7f2d59938636e157e6982e989f
Hiroyuki Furuno <58550284+HiroyukiFuruno@users.noreply.github.com>
2026-04-22 11:44:59 +0900

    chore: update katana-desktop to 0.22.3

--- Casks/katana-desktop.rb
+++ Casks/katana-desktop.rb
@@ -1,8 +1,8 @@
 cask "katana-desktop" do
-  version "0.22.2"
-  sha256 "cef3c6c54dbca83467f148d701b118d97e0b7792511fa0b5b70026044de2729e"
+  version "0.22.3"
+  sha256 "7499d63496a4f66fe3de2271a3faf3d96829ee009438c08235c5b01caf420110"
 
-  url "https://github.com/HiroyukiFuruno/KatanA/releases/download/v0.22.2/KatanA-Desktop-0.22.2.dmg"
+  url "https://github.com/HiroyukiFuruno/KatanA/releases/download/v0.22.3/KatanA-Desktop-0.22.3.dmg"
   name "KatanA Desktop"
   desc "Lightweight Markdown viewer with live preview, Mermaid diagrams, and syntax highlighting"
   homepage "https://github.com/HiroyukiFuruno/KatanA"
