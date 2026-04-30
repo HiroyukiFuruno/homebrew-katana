93d5d1c4500486695aae88a3934c3ac58904cf1c
Hiroyuki Furuno <58550284+HiroyukiFuruno@users.noreply.github.com>
2026-05-01 08:28:49 +0900

    chore: add kml@0.17.6.rb

--- Formula/kml@0.17.6.rb
+++ Formula/kml@0.17.6.rb
@@ -9,18 +9,18 @@ class KmlAT0176 < Formula
   on_macos do
     on_arm do
       url "https://github.com/HiroyukiFuruno/katana-markdown-linter/releases/download/v0.17.6/kml-v0.17.6-aarch64-apple-darwin.tar.gz"
-      sha256 "df94f1cbbf1a7c1ce4ecbbd89a3936bd9e78fe2cc59d69a70c9f31f8118204df"
+      sha256 "398418b9dfa9351c26dcd05a4d9b6a84b46ec5e1865f018f5e0f752bf4ef865b"
     end
     on_intel do
       url "https://github.com/HiroyukiFuruno/katana-markdown-linter/releases/download/v0.17.6/kml-v0.17.6-x86_64-apple-darwin.tar.gz"
-      sha256 "1c1834675bb833e6159734e6b833f31fded3e394002636b7d4d87639ab6c12a3"
+      sha256 "62d0ce84086b98c9fde483337102ed68cf7026719bb4976f0ff0e14461d8011b"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/HiroyukiFuruno/katana-markdown-linter/releases/download/v0.17.6/kml-v0.17.6-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "7a3ce163f956e881c98f4df1c173554d415871e653b39ac97a18f00d005759bf"
+      sha256 "33885006de0df19306dcab3813648915f54250d5f957ef5a3c6fa2fd32a5c3ee"
     end
   end
 
