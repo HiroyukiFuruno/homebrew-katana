fd7867ca8a659ba9c16a2085ab002a145de49ebd
Hiroyuki Furuno <58550284+HiroyukiFuruno@users.noreply.github.com>
2026-07-04 11:53:59 +0900

    chore: add katana-desktop@0.29.0 (0.29.0)

---
+++ Formula/katana-desktop@0.29.0.rb
@@ -0,0 +1,16 @@
+class KatanaDesktopAT0290 < Formula
+  desc "Lightweight Markdown viewer with live preview, Mermaid diagrams, and syntax highlighting"
+  homepage "https://github.com/HiroyukiFuruno/KatanA"
+  version "0.29.0"
+
+  url "https://github.com/HiroyukiFuruno/KatanA/releases/download/v0.29.0/KatanA-linux-x86_64.tar.gz"
+  sha256 "e6120a71a41c7d33af010df9ff6e2a865be664e3f899e3a49fe9a3651eb0da0e"
+
+  depends_on :linux
+
+  link_overwrite "bin/katana-desktop"
+
+  def install
+    bin.install "KatanA" => "katana-desktop"
+  end
+end
