2f378dd461f199ec37f5cf98a544759068099dcd
Hiroyuki Furuno <58550284+HiroyukiFuruno@users.noreply.github.com>
2026-09-26 06:43:28 +0900

    chore: update kml to 0.19.5

--- Formula/kml.rb
+++ Formula/kml.rb
@@ -1,24 +1,24 @@
 class Kml < Formula
   desc "Markdownlint-compatible Markdown linter library and CLI"
   homepage "https://github.com/HiroyukiFuruno/katana-markdown-linter"
-  version "0.19.4"
+  version "0.19.5"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/HiroyukiFuruno/katana-markdown-linter/releases/download/v0.19.4/kml-v0.19.4-aarch64-apple-darwin.tar.gz"
-      sha256 "5a6dbedfd3a32a081a53c575cee2ae1d3d616de81e9641aa0c252d136410de47"
+      url "https://github.com/HiroyukiFuruno/katana-markdown-linter/releases/download/v0.19.5/kml-v0.19.5-aarch64-apple-darwin.tar.gz"
+      sha256 "021b1e48b9d2fb18680b6262ba0ae10ba243a9d07874ed9111e47e76109e98e7"
     end
     on_intel do
-      url "https://github.com/HiroyukiFuruno/katana-markdown-linter/releases/download/v0.19.4/kml-v0.19.4-x86_64-apple-darwin.tar.gz"
-      sha256 "0046b61d09173f10b11cb10e36e25503be527182b8f17db37322e1b9fc715caa"
+      url "https://github.com/HiroyukiFuruno/katana-markdown-linter/releases/download/v0.19.5/kml-v0.19.5-x86_64-apple-darwin.tar.gz"
+      sha256 "9358ca63f8add3b13bfe0ec8af11950602d25047189270705c8064074067aae0"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/HiroyukiFuruno/katana-markdown-linter/releases/download/v0.19.4/kml-v0.19.4-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "ea337721b1a33b9287cbac9a8df68af504de559bae67a340a2ef09a49442c591"
+      url "https://github.com/HiroyukiFuruno/katana-markdown-linter/releases/download/v0.19.5/kml-v0.19.5-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "66d8b9af2cba8f524eedda5c937ea673cb62e9fa8b7a7fcd06c475dd4a6bc4ec"
     end
   end
 
